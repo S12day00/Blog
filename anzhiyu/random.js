0fb4344928c2af759fe1ced4300cc89e1dba1498
S12day00 <S12day00@users.noreply.github.com>
2025-06-22 21:56:15 +0000

    deploy: S12day00/Blog@8905da43dbad9431215bd859ce7e97920eb69ee1

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/23/hello-world/"];function toRandomPost(){
+var posts=["2025/06/22/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
